0a861e53064305c3cf35debfdb635c8c82cb2304
admin <fraz@gmail.com>
2024-08-03 12:32:37 +0500

    feat: values

---
+++ 02-values/main.go
@@ -0,0 +1,22 @@
+/**
+Go has various value types including strings, integers, floats, booleans
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	//strings can be added with +
+	fmt.Println("Go" + "lang")
+
+	// integers can be added
+	fmt.Println(1 + 1)
+
+	// floats
+	fmt.Println(3.1 / 2.1)
+
+	// booleans
+	fmt.Println(true && false)
+
+}
